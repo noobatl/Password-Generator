3afa9cc60b73b8e88ae4ff73e220b281bb10472f
noobatl <info@noobusa.com>
2020-03-06 23:16:21 -0500

    added functions to randomize letters,numbers,symbols; prompt and alert commands; console.log information

--- script.js
+++ script.js
@@ -1,40 +1,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+
 // Write password to the #password input
 function generatePassword() {
-  return "Something";
-}
-  // console.log("Generating my password!");
-  // Prompt the user for password length. 
-  // Check to see if the user actually entered a length. 
-  // 4 Confirms
-  // 1) special characters
-  // 2) numbers
-  // 3) lower case
-  // 4) Uppercase
-  // Generate password, based on user specifications. 
-  // NOTE: User specifications come from confirms. **Save them in variables**
-  //
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+  console.log("Generating my password!");
+  var passwordChar = prompt("How many characters do you want in your password?");
 
-  for(var i = 0; i < 10; i++){
+  if (passwordChar > 7 && passwordChar < 129 ){
+    confirm ("Your password will have " + passwordChar + " characters.");
+    } else {
+    alert ("Please enter a NUMBER between 8 and 128.")
+    return "Please enter a NUMBER between 8 and 128.";    
+  }
+
+
+  var addLower = confirm("Do you want to include lowercase letters in your password?");
+
+  if (addLower){
+    var setLower = function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    console.log (setLower); 
+  }
+
+  var addUpper = confirm("Do you want to include uppercase letters in your password?");
+
+  if (addUpper){
     
   }
+
+  var addNumber = confirm("Do you want to include numbers in your password?");
+
+  if (addNumber){
+    
+  }
+
+  var addSymbol = confirm("Do you want to include symbols in your password?");
+
+  if (addSymbol){
+     
+  }
+}
+
+//Add function to generate random lowercase letters
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+// }
+
+//Add function to generate random uppercase letters
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
+}
+
+//Add function to generate random numbers 0-9
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
+}
+
+//Add funtion to generate random symbols from (https://www.owasp.org/index.php/Password_special_characters).
+//OPTION 1 - using arrays
+// var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+// function getRandomSymbol() {
+//   for (var i = 0; i < symbols.length; i++){
+//     return symbols[Math.floor(Math.random() * symbols.length)];
+//   }
+// }
+//OPTION 2 - found cleaner way to get symbols using constants declaration
+function getRandomSymbol() {
+  const symbols = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// function generatePassword() {
+//     var string = ""; 
+//     var length = charset.length,
+//         retVal = "";
+//     for (var i = 0; i < [user-defined-password-length-variable]; ++i) {
+//         retVal += charset.charAt(Math.floor(Math.random() * length));
+//     }
+//     return retVal;
+// }
+  
+
+// for(var i = 0; i < 10; i++){
+    
+//   }
 
 
 
@@ -48,3 +102,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
+
